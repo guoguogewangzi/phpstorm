4584429a350020520e3c6d6b75535fa8e02b3c9d
zhangxin <879086359@qq.com>
2021-11-15 00:00:49 +0800

    add jsonp dir

---
+++ phpstorm/jsonp/remote.js
@@ -0,0 +1,18 @@
+
+// 把该文件放在远程服务器上，如：http://175.24.115.4:8000/remote.js
+//test({"result":""});
+
+
+
+test({
+    "result":"175.24.115.4"
+});
+
+
+
+flightHandler({
+    "code": "CA1998",
+    "price": 1780,
+    "tickets": 5
+});
+
